fb848047af33f27c4417bd5990dff034dfe675ea
alswns133 <112874894+alswns133@users.noreply.github.com>
2026-05-11 21:34:51 +0900

    2026-05-11 (#12)

--- Assets/Scripts/GoalBox.cs
+++ Assets/Scripts/GoalBox.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using System.Collections;
 
 public class GoalBox : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject goalObj;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.name == "Key")
+        {
+            Animator animator = GetComponent<ObjectMovement>().ObjAnimator;
+            animator.SetTrigger("Goal");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // 애니메이션 이벤트에서 호출
+    private void Goal()
     {
-        
+        Vector2 goalPos = transform.position;
+        goalPos.y -= 0.5f;
+        Instantiate(goalObj, goalPos, Quaternion.identity);
+    }
+
+    // 애니메이션 이벤트에서 호출
+    private void DestroyGoalBox()
+    {
+        Destroy(gameObject);
     }
 }
