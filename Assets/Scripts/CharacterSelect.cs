a9bc02453e4eba16bffdb7942e34fb7ab3ee8960
KimMinjun <rlaalswns143@gmail.com>
2026-05-12 16:58:32 +0900

    워리어 공격 애니메이션 추가
    워리어 공격 애니메이션추가
    캐릭터 변경 스크립트 추가 및 적용(이펙트 미구현)
    캐릭터 변경 UI 스크립트 추가 및 적용

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public enum Character
 {
@@ -17,11 +14,13 @@ public class CharacterSelect : MonoBehaviour
     // 받아올 Player 리스트
     [SerializeField] private List<Player> players;
     // Dictionary로 오브젝트 이름을 받아서 그것의 Player를 받아 쓸 용도
-    private Dictionary<string, Player> playerDictionary = new Dictionary<string, Player>();
+    private Dictionary<Character, Player> playerDictionary = new Dictionary<Character, Player>();
     // 현재 Player의 포지션
-    [SerializeField] private Vector3 currentPlayerPos;
+    [SerializeField] private Transform currentPlayerTransform;
     // 현재 캐릭터
     [SerializeField] private Character currentCharacter;
+    // 캐릭터 선택 UI
+    [SerializeField] private CharacterSelectUI characterSelectUI;
 
     void Start()
     {
@@ -33,57 +32,55 @@ public class CharacterSelect : MonoBehaviour
     {
         players.AddRange(GetComponentsInChildren<Player>(true));
 
-        for(int i = 0; i < players.Count; i++)
+        characterSelectUI = GameObject.FindAnyObjectByType<CharacterSelectUI>();
+        characterSelectUI?.Initialize();
+        characterSelectUI?.SelectUI(currentCharacter);
+
+        for (int i = 0; i < players.Count; i++)
         {
-            playerDictionary.Add(players[i].gameObject.name, players[i]);
+            playerDictionary.Add(players[i].PlayerCharacterType, players[i]);
         }
+
+        Characters(Character.Thief);
     }
 
     // 현재 포지션값을 받을 메서드
-    private void SetPlayerPos(Player player)
+    private Transform SetPos(Player player)
     {
-        currentPlayerPos = player.Pos;
+        currentPlayerTransform = player.Transform;
+
+        return currentPlayerTransform;
     }
 
     private void Characters(Character c)
     {
         // 예외처리
         if (currentCharacter == c) return;
-
-        Player currentPlayer = playerDictionary[currentCharacter.ToString()];
-
-        SetPlayerPos(currentPlayer);
-
-        Player nextPlayer = playerDictionary[c.ToString()];
-
+        // 현재 캐릭터
+        Player currentPlayer = playerDictionary[currentCharacter];
+        // 바꿀 캐릭터
+        Player nextPlayer = playerDictionary[c];
+        // 캐릭터 전체를 비활성화
         foreach(Player player in  players)
         {
             player.gameObject.SetActive(false);  
         }
+        // 다음 캐릭터 활성화
         nextPlayer.gameObject.SetActive(true);
-
+        // 위치 값 초기화
+        nextPlayer.SetPlayerTransform(SetPos(currentPlayer));
+        // 현재 캐릭터 업데이트
         currentCharacter = c;
+        // 현재 캐릭터 선택 UI 변경
+        characterSelectUI.SelectUI(currentCharacter);
+
     }
 
     private void CharacterChange()
     {
-        string input = Input.inputString;
-
-        if (!string.IsNullOrEmpty(input))
-        {
-            switch (input)
-            {
-                case "1":
-                    Characters(Character.Warrior);
-                    break;
-                case "2":
-                    Characters(Character.Thief);
-                    break;
-                case "3":
-                    Characters(Character.Wizard);
-                    break;
-            }
-        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Characters(Character.Warrior);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) Characters(Character.Thief);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) Characters(Character.Wizard);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField] Player player;
+
+    private void Start()
+    {
+        Initialize();
+    }
+    private void Initialize()
+    {
+        player = FindAnyObjectByType<Player>();
+        player.IsMoving(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
