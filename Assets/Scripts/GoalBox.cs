d39a7a8e32e70cdc0f9040c99d625722eb43da4f
anyanswer1125 <82665726+anyanswer1125@users.noreply.github.com>
2026-05-14 10:02:21 +0900

    Key -> TreasureBox 결합 시 isMoving 꺼지지 않던 문제 해결

--- Assets/Scripts/GoalBox.cs
+++ Assets/Scripts/GoalBox.cs
@@ -4,13 +4,19 @@ using System.Collections;
 public class GoalBox : MonoBehaviour
 {
     [SerializeField] private GameObject treasureObj;
-    
+    [SerializeField] private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.name == "Key")
         {
             Animator animator = GetComponent<ObjectMovement>().ObjAnimator;
+            audioSource.Play();
             animator.SetTrigger("Goal");
         }
     }
@@ -22,6 +28,7 @@ public class GoalBox : MonoBehaviour
         goalPos.y -= 0.5f;
         // Instantiate(goalObj, goalPos, Quaternion.identity);
         treasureObj.transform.position = goalPos;
+        treasureObj.SetActive(true);
         
 
     }
